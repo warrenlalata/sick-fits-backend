c4f19a0c19aa548524d0984455e6a5239702a753
Mark Warren Lalata <markwarren.dev@gmail.com>
2019-01-10 16:17:49 +0800

    initial graphql yoga server

---
+++ src/createServer.js
@@ -0,0 +1,19 @@
+const { GraphQLServer } = require('graphql-yoga')
+
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const db = require('./db')
+
+// Create GraphQL Yoga server
+function createServer() {
+    return new GraphQLServer({
+        typeDefs: 'src/schema.graphql',
+        resolvers: {
+            Mutation,
+            Query
+        },
+        context: req => ({ ...req, db }), // attach db into every req
+    })
+}
+
+module.exports = createServer
