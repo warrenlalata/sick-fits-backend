986e0b5afb2eeaf2484024f2ad601634a37e2647
Mark Warren Lalata <markwarren.dev@gmail.com>
2019-01-11 10:40:06 +0800

    initial createItem Mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,3 +1,17 @@
-const Mutations = {}
+const Mutations = {
+    async createItem(parent, args, ctx, info) {
+        // TODO check if there is logged in user
+
+        // db.mutation is a prisma-binding method, see: https://github.com/prisma/prisma-binding
+        // createItem() is referred in 'src/generated/prisma.graphql' inside type Mutation {}
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args, // same as title: args.title, description: args.description...
+            }
+        }, info) // info is passed as second arg to make sure that the actual item created is returned
+        
+        return item
+    }
+}
 
 module.exports = Mutations
