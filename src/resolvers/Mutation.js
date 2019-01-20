616c0eb8f65ee0be73aa70d691e2dda127ac4c1b
Mark Warren Lalata <markwarren.dev@gmail.com>
2019-01-20 21:44:04 +0800

    siginin mutation resolver

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -2,69 +2,91 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 const Mutations = {
-    async createItem(parent, args, ctx, info) {
-        // TODO check if there is logged in user
+  async createItem(parent, args, ctx, info) {
+    // TODO check if there is logged in user
 
-        // db.mutation is a prisma-binding method, see: https://github.com/prisma/prisma-binding
-        // createItem() is referred in 'src/generated/prisma.graphql' inside type Mutation {}
-        const item = await ctx.db.mutation.createItem({
-            data: {
-                ...args, // same as title: args.title, description: args.description...
-            }
-        }, info) // info is passed as second arg to make sure that the actual item created is returned
-        
-        return item
-    },
+    // db.mutation is a prisma-binding method, see: https://github.com/prisma/prisma-binding
+    // createItem() is referred in 'src/generated/prisma.graphql' inside type Mutation {}
+    const item = await ctx.db.mutation.createItem({
+        data: {
+            ...args, // same as title: args.title, description: args.description...
+        }
+    }, info) // info is passed as second arg to make sure that the actual item created is returned
+    
+    return item
+  },
 
-    updateItem(parent, args, ctx, info) {
-        // copy the args
-        const updates = { ...args }
-        // remove the ID because we do not want to update the 
-        // id of the item!
-        delete updates.id
-        // run the update method
-        return ctx.db.mutation.updateItem({
-            data: updates,
-            where: {
-                id: args.id
-            },
-        }, info)
-    },
+  updateItem(parent, args, ctx, info) {
+    // copy the args
+    const updates = { ...args }
+    // remove the ID because we do not want to update the 
+    // id of the item!
+    delete updates.id
+    // run the update method
+    return ctx.db.mutation.updateItem({
+        data: updates,
+        where: {
+            id: args.id
+        },
+    }, info)
+  },
 
-    async deleteItem(parent, args, ctx, info) {
-        const where = { id: args.id }
-        // find the item
-        const item = await ctx.db.query.item({ where }, `{ id title }`) // manually request `{id title}`
-        // check if they own the item or has permission
-        // delete it
-        return ctx.db.mutation.deleteItem({ where }, info)
-    },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id }
+    // find the item
+    const item = await ctx.db.query.item({ where }, `{ id title }`) // manually request `{id title}`
+    // check if they own the item or has permission
+    // delete it
+    return ctx.db.mutation.deleteItem({ where }, info)
+  },
 
-    async signup(parent, args, ctx, info) {
-        // lowercase their email
-        args.email = args.email.toLowerCase()
-        // hash password
-        const password = await bcrypt.hash(args.password, 10)
-        
-        const user = await ctx.db.mutation.createUser({
-            data: { 
-                ...args,
-                password,
-                permissions: { set: ['USER'] } // user set because Permission is enum type!
-            }
-        }, info)
+  async signup(parent, args, ctx, info) {
+    // lowercase their email
+    args.email = args.email.toLowerCase()
+    // hash password
+    const password = await bcrypt.hash(args.password, 10)
+    
+    const user = await ctx.db.mutation.createUser({
+        data: { 
+            ...args,
+            password,
+            permissions: { set: ['USER'] } // user set because Permission is enum type!
+        }
+    }, info)
 
-        // create a JWT 
-        const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
-        // set cookie on response
-        ctx.response.cookie('token', token, {
-            httpOnly: true,
-            maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
-        })
-        // finally, return newly created user
-        return user
+    // create a JWT 
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+    // set cookie on response
+    ctx.response.cookie('token', token, {
+        httpOnly: true,
+        maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    })
+    // finally, return newly created user
+    return user
+  },
 
+  async signin(parent, { email, password }, ctx, info) {
+    // check if there is user with that email
+    const user = await ctx.db.query.user({ where: { email } })
+    if (!user) {
+        throw new Error('User not found')
     }
+    console.log(user)
+    // check is password is correct
+    const valid = await bcrypt.compare(password, user.password)
+    if (!valid) {
+        throw new Error('Invalid password')
+    }
+    // generate the jwt
+    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+    // set cookie with token
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+    })
+    // return user
+    return user
+  }
 }   
 
 module.exports = Mutations
