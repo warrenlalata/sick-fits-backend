974408b3bfb1d5038fe83d146a8a77fea2df17d6
Mark Warren Lalata <markwarren.dev@gmail.com>
2019-01-20 18:49:25 +0800

    currentUser query resolver

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,13 +1,23 @@
 const { forwardTo } = require('prisma-binding')
 
 const Query = {
-    //items: forwardTo('db'),
-    async items(parent, args, ctx, info) {
-        const items = await ctx.db.query.items({ ...args })
-        return items
-    },
-    item: forwardTo('db'),
-    itemsConnection: forwardTo('db')
+  //items: forwardTo('db'),
+  async items(parent, args, ctx, info) {
+    const items = await ctx.db.query.items({ ...args })
+    return items
+  },
+  item: forwardTo('db'),
+  itemsConnection: forwardTo('db'),
+  currentUser(parent, args, ctx, info) {
+    // check if there is a current userId
+    if (!ctx.request.userId) {
+      return null
+    }
+    return ctx.db.query.user({
+      where: { id: ctx.request.userId }
+    }, info)
+
+  }
 }
 
 module.exports = Query
