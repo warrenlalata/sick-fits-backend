155c1607398a11bdd84ecb31a6a25aebe342ec8f
Mark Warren Lalata <markwarren.dev@gmail.com>
2019-01-20 22:36:02 +0800

    signout mutation

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -8,9 +8,9 @@ const Mutations = {
     // db.mutation is a prisma-binding method, see: https://github.com/prisma/prisma-binding
     // createItem() is referred in 'src/generated/prisma.graphql' inside type Mutation {}
     const item = await ctx.db.mutation.createItem({
-        data: {
-            ...args, // same as title: args.title, description: args.description...
-        }
+      data: {
+        ...args, // same as title: args.title, description: args.description...
+      }
     }, info) // info is passed as second arg to make sure that the actual item created is returned
     
     return item
@@ -24,10 +24,10 @@ const Mutations = {
     delete updates.id
     // run the update method
     return ctx.db.mutation.updateItem({
-        data: updates,
-        where: {
-            id: args.id
-        },
+      data: updates,
+      where: {
+        id: args.id
+      },
     }, info)
   },
 
@@ -47,46 +47,51 @@ const Mutations = {
     const password = await bcrypt.hash(args.password, 10)
     
     const user = await ctx.db.mutation.createUser({
-        data: { 
-            ...args,
-            password,
-            permissions: { set: ['USER'] } // user set because Permission is enum type!
-        }
+      data: { 
+        ...args,
+        password,
+        permissions: { set: ['USER'] } // user set because Permission is enum type!
+      }
     }, info)
 
     // create a JWT 
     const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
     // set cookie on response
     ctx.response.cookie('token', token, {
-        httpOnly: true,
-        maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
     })
     // finally, return newly created user
     return user
   },
 
-  async signin(parent, { email, password }, ctx, info) {
-    // check if there is user with that email
-    const user = await ctx.db.query.user({ where: { email } })
-    if (!user) {
-        throw new Error('User not found')
-    }
-    console.log(user)
-    // check is password is correct
-    const valid = await bcrypt.compare(password, user.password)
-    if (!valid) {
-        throw new Error('Invalid password')
-    }
-    // generate the jwt
-    const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
-    // set cookie with token
-    ctx.response.cookie('token', token, {
-      httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
-    })
-    // return user
-    return user
+async signin(parent, { email, password }, ctx, info) {
+  // check if there is user with that email
+  const user = await ctx.db.query.user({ where: { email } })
+  if (!user) {
+    throw new Error('User not found')
   }
-}   
+  console.log(user)
+  // check is password is correct
+  const valid = await bcrypt.compare(password, user.password)
+  if (!valid) {
+    throw new Error('Invalid password')
+  }
+  // generate the jwt
+  const token = jwt.sign({ userId: user.id }, process.env.APP_SECRET)
+  // set cookie with token
+  ctx.response.cookie('token', token, {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year cookie
+  })
+  // return user
+  return user
+},
+
+  signout(parent, args, ctx, info) {
+    ctx.response.clearCookie('token') //clearCookie is methid from cookie-parser middleware
+    return { message: 'Signed out!' }
+  }
+} 
 
 module.exports = Mutations
