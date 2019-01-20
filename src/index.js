ba097a0eb2c4d19a66f334927b5820deafa4bb7d
Mark Warren Lalata <markwarren.dev@gmail.com>
2019-01-20 18:49:08 +0800

    verify jwt middleware

--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 require('dotenv').config({ path: '.env' })
 const cookieParser = require('cookie-parser')
+const jwt = require('jsonwebtoken')
 
 const createServer = require('./createServer')
 const db = require('./db')
 
 const server = createServer()
 
-// Todo use express middleware to handle cookies
+// express middleware to handle cookies
 server.express.use(cookieParser())
-// TODO use express middleware to populate current user
+  // decode JWT so we can get the userId
+  server.express.use((req, res, next) => {
+  const { token } = req.cookies
 
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET)
+    // attach userId to request
+    req.userId = userId
+  }
+
+  next()
+})
 
 server.start({
-    cors: {
-        credentials: true,
-        origin: process.env.FRONTEND_URL
-    },
+  cors: {
+    credentials: true,
+    origin: process.env.FRONTEND_URL
+  },
 }, ({ port }) => console.log(`Server running at http://localhost:${port}`))
